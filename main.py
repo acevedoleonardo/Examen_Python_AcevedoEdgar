aad395526eddd2f0af430b39a9133ef8b4fd8f1b
acevedoleonardo <leonardo1191@gmail.com>
2025-02-13 08:56:54 -0500

    Examen

---
+++ main.py
@@ -0,0 +1,21 @@
+#Algotimo Principal 
+#Creacion de un CRUD 
+
+print("#####################################################################")
+print(" Bienvenidos a Movistar ")
+print("#####################################################################")
+print("...Registros de Usuarios...")
+print("#####################################################################")
+print("Seleccione su opcion")
+print("---------------------------------------------------------------------")
+print("1. Agregar usuario nuevo ")
+print("2. Visualizar usuarios registrados ")
+print("3. Modificar usuarios registrados ")
+print("4. Eliminar usuarios registrados ")
+print("5. Salir")
+opcion=input("\n :  ")                                                                                      #Cree una variable opcion para guardar el dato que me de el usuario para ingrersar al menu 
+
+
+
+
+
---
+++ registros_clientes.py
@@ -0,0 +1,52 @@
+#Registro y Gestión de Usuarios 
+
+import json 
+
+def abrirJSON(): 
+    dicFinal={}
+    with open("./registros.json","r") as openFile:
+        dicFinal=json.load(openFile)
+    return dicFinal
+
+def guardarJSON():
+    with open("./registros.json","w") as outFile:
+        json.dump("dic",outFile)
+    
+registros={}
+
+
+
+
+print("---------------------------------------------------------")
+print("Registro de Clientes")
+print("---------------------------------------------------------")
+nombre=input("Ingrese su Nombre: ")
+direccion=input("Ingrese su Direccion: ")
+telefono=input("Ingrese su Telefono: ")
+tiempo=input("¿Cuantos años tiene con la compañia movistar?: ")
+registros=abrirJSON()
+if (tiempo<="1"):
+    print("==Categorias==")
+    print("Nuevos Clientes")
+elif (tiempo>"1" and tiempo <="3"):
+    print("==Categorias==")
+    print("Clientes Regulares")
+else:
+    print("==Categorias==")
+    print("Clientes Leales")
+
+
+#Abrir Json
+
+registros=abrirJSON()
+
+registros.append({"nombre":nombre,
+                           "direccion":direccion,
+                           "telefono":telefono,
+                           "tiempo":tiempo
+                            })
+
+#Guardar diccionario en el archivo JSON
+guardarJSON(registros)
+print("Usuario Agregado Exitosamente")
+
